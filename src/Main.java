1e3b540f463c1283666f7a7ddd81b51f7d1df1a7
Administrator <Administrator@MSDN-SPECIAL>
2018-10-07 00:08:58 +0900

    출력 구현 (10/6~7)

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,20 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.TreeMap;
 
-class Car{
+class Text{
     String author;
     String date;
     String text;
     String favorite;
-    HashSet<String> terms = new HashSet<>();
+    HashSet<String> terms = new HashSet<>(); // 단어의 중복 저장을 막기 위해 Set으로 구현
 
-    public Car(String author,String date,String text,String favorite){
+    public Text(String author,String date,String text,String favorite){
         this.author=author;
         this.date=date;
         this.text=text;
         this.favorite=favorite;
         makeTerm();
-        System.out.println(terms);
+//        System.out.println(terms);
     }
 
     @Override
@@ -48,9 +48,9 @@ class Car{
 }
 
 public class Main{
-    static int count=0;
-    static ArrayList<Car> carList = new ArrayList<Car>();
-    static TreeMap<String,ArrayList<Integer>> dictionary = new TreeMap<>();
+    static int count=0,sum=0,max=-1,min=-1;
+    static ArrayList<Text> carList = new ArrayList<Text>();
+    static TreeMap<String,ArrayList<Integer>> dictionary = new TreeMap<>(); // 사전은 균형이진트리로 만들어진 TreeMap을 만들어 구현
 
     public static void inputData(String file) throws FileNotFoundException {
         Scanner input = new Scanner(new File(file));
@@ -70,14 +70,19 @@ public class Main{
             favorite=favorite.replaceAll("<FAVORITE>","");
             favorite=favorite.replaceAll("</FAVORITE>","");
             input.nextLine();
-            Car c = new Car(author,date,text,favorite);
-            System.out.println(c+"\n\n");
+            Text c = new Text(author,date,text,favorite);
+            int size=c.terms.size();
+            sum+=size;
+            if(max==-1|size>max) max=size;
+            if(min==-1|size<min) min=size;
+//            System.out.println(c+"\n\n");
             carList.add(c);
             count++;
         }
     }
 
     public static void main(String[]args)  throws FileNotFoundException {
+        Scanner input = new Scanner(System.in);
         inputData("2007_acura_mdx");
         inputData("2007_acura_rl");
         inputData("2007_acura_tl");
@@ -99,8 +104,8 @@ public class Main{
         inputData("2007_chevrolet_corvette");
         inputData("2007_chevrolet_impala");
         inputData("2007_chevrolet_malibu");
-        System.out.println("Number of Car : "+count);
-        System.out.println("Number of Car : "+carList.size());
+//        System.out.println("Number of Car : "+count);
+//        System.out.println("Number of Car : "+carList.size());
         for(int i=0;i<carList.size();i++){
             String[] set=carList.get(i).terms.toArray(new String[carList.get(i).terms.size()]);;
             for(int j=0;j<set.length;j++){
@@ -116,10 +121,29 @@ public class Main{
                 }
             }
 //            System.out.println();
+            // 트리맵의 키 값을 단어, 벨류값을 해당 단어의 포스팅 리스트로 사전을 트리에 구현
         }
         System.out.println();
-        System.out.println(dictionary.get("this"));
-        System.out.println(carList.get(0));
+        System.out.println("문서의 수 : "+count);
+        System.out.println("문서당 평균 단어 수 : "+(sum/count));
+        System.out.println("문서의 포함된 단어 중 가장 큰 수와 작은 수: "+max+","+min);
+        System.out.println();
+        System.out.print("원하는 단어를 입력하세요 : ");
+        ArrayList postingList = dictionary.get(input.next());
+        if(postingList==null){
+            System.out.println("None");
+        }
+        else{
+            System.out.println(postingList);
+            System.out.print("원하는 문서의 id를 입력하세요 : ");
+            int id = input.nextInt();
+            if(postingList.contains(id)){
+                System.out.println(carList.get(id));
+            }
+            else {
+                System.out.println("문서가 존재하지 않거나 해당 단어가 없는 문서 입니다.");
+            }
+        }
     }
 
 }
